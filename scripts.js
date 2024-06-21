0a990ca0f732cdf3459f275d7da267d85696f1aa
shakevo <monochrome0329@gmail.com>
2024-06-21 15:34:06 +0900

    コメント追加, キャッチコピーのセクション名変更

--- scripts.js
+++ scripts.js
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     links.forEach(link => {
         link.addEventListener('click', function(event) {
+            // 既定のリンク動作をキャンセル
             event.preventDefault();
+            // ターゲットIDを取得
             const targetId = event.target.getAttribute('href').substring(1);
+            // ターゲットIDを元にターゲットセクションを取得
             const targetSection = document.getElementById(targetId);
 
             window.scrollTo({
@@ -15,18 +18,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    
+    // アコーディオンボタンを取得
     const accordionButtons = document.querySelectorAll('.accordion button');
 
     accordionButtons.forEach(button => {
         button.addEventListener('click', function() {
+            // 展開状態を取得
             const expanded = this.getAttribute('aria-expanded') === 'true' || false;
+            // 展開状態を切り替える
             this.setAttribute('aria-expanded', !expanded);
 
+            // ボタンの要素(コンテンツ)を取得
             const content = this.nextElementSibling;
             if (!expanded) {
+                // 展開する場合
                 content.style.display = 'block';
             } else {
+                // 閉じる場合
                 content.style.display = 'none';
             }
         });
